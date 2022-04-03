513b686073a2d63988ed834e66d9c24d1f569f6c
gallapillo <58660325+gallapillo@users.noreply.github.com>
2022-04-03 10:51:24 +0700

    coin list screen

---
+++ app/src/main/java/com/gallapillo/crytpoapp/presentation/Screen.kt
@@ -0,0 +1,6 @@
+package com.gallapillo.crytpoapp.presentation
+
+sealed class Screen(val route: String) {
+    object CoinListScreen: Screen("coin_list_screen")
+    object CoinDetailScreen: Screen("coin_detail_screen")
+}
